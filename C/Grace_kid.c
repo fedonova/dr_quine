2cf59e8a10108226608e0f0db7987d428d8c62cd
rtire <fedonova@gmail.com>
2025-01-09 04:18:45 +0100

    lalalala

--- C/Grace_kid.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-// commentaire
-#define SOURCE_FILE "Grace_kid.c"
-#define SOURCE_CODE "#include <stdio.h>%c// commentaire%c#define SOURCE_FILE %cGrace_kid.c%c%c#define SOURCE_CODE %c%s%c%c#define FT(x) int main(){FILE *fd = fopen(x, %cw%c);if (fd == NULL) return 0;fprintf(fd, SOURCE_CODE,10,10,34,34,10,34,SOURCE_CODE,34,10,34,34,10,10);fclose(fd);}%cFT(SOURCE_FILE)%c"
-#define FT(x) int main(){FILE *fd = fopen(x, "w");if (fd == NULL) return 0;fprintf(fd, SOURCE_CODE,10,10,34,34,10,34,SOURCE_CODE,34,10,34,34,10,10);fclose(fd);}
-FT(SOURCE_FILE)
