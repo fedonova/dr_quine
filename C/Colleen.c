670f47898aafd113d69cdd81325886642a0ee355
rtire <fedonova@gmail.com>
2025-01-17 18:20:10 +0100

    provero4ki

--- C/Colleen.c
+++ C/Colleen.c
@@ -6,8 +6,8 @@ Salut !
 
 void print_source(void)
 {
-	char *source = "#include <stdio.h>%c%c/*%cSalut !%c*/%c%cvoid print_source(void)%c{%c%cchar *source = %c%s%c;%c%cprintf(source, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, source, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10);%c}%c%cint main(void)%c{%c%c/*%c%cC'est un commentaire dans la fonction main%c%c*/%c%cprint_source();%c%creturn 0;%c}";
-	printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, source, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10);
+	char *source = "#include <stdio.h>%c%c/*%cSalut !%c*/%c%cvoid print_source(void)%c{%c%cchar *source = %c%s%c;%c%cif (printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, source, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10) < 0) return;%c}%c%cint main(void)%c{%c%c/*%c%cC'est un commentaire dans la fonction main%c%c*/%c%cprint_source();%c%creturn 0;%c}";
+	if (printf(source, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, source, 34, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10) < 0) return;
 }
 
 int main(void)
